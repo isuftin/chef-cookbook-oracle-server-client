f8d392dcb9ea8a5a5205439e105ba9d9f27a76ce
Ivan Suftin <isuftin@usgs.gov>
2016-09-29 13:16:26 -0500

    Version change

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,5 @@
 
 This file is used to list changes made in each version of the owi-oracle-server-client cookbook.
 
-## 1.2.2-1
+## 0.0.1
 - [isuftin@usgs.gov] - Initial commit
--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'isuftin@usgs.gov'
 license          'Public Domain'
 description      'Installs/Configures an 12.x Oracle Server and client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.2-1'
+version          '0.0.1'
 
 source_url 'https://github.com/USGS-CIDA/chef-cookbook-oracle-server-client' if respond_to?(:source_url)
 issues_url 'https://github.com/USGS-CIDA/chef-cookbook-oracle-server-client/issues' if respond_to?(:issues_url)
 
-depends 'oracle'
+depends 'oracle', '= 1.2.2'
